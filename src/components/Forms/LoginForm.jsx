3314e3eaeaa1225f4de22863908e152ab990d98d
pocketful <iveta.jac@gmail.com>
2022-07-04 16:23:16 +0300

    Login validation with Yup

--- src/components/Forms/LoginForm.jsx
+++ src/components/Forms/LoginForm.jsx
@@ -1,4 +1,5 @@
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import Button from '../UI/Button/Button';
 import style from './LoginForm.module.css';
 
@@ -10,10 +11,16 @@ const initialValues = {
 function LoginForm() {
   const formik = useFormik({
     initialValues,
+    validationSchema: Yup.object({
+      email: Yup.string().email('invalid email address').required(),
+      password: Yup.string().min(4, 'min 4 characters').max(20).required(),
+    }),
     onSubmit: async (values) => {
       console.log('submitted values: ', values);
     },
   });
+
+  console.log('formik.errors:', formik.errors);
 
   return (
     <div className={style.wrapper}>
@@ -21,25 +28,37 @@ function LoginForm() {
       <form onSubmit={formik.handleSubmit}>
         <div className={style.group}>
           <input
-            type="email"
+            // type="email" // TODO
+            type="text"
             name="email"
             placeholder="Email"
-            className={style.input}
+            className={`${style.input} ${
+              formik.errors.email ? style.inputErr : ''
+            }`}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             value={formik.values.email}
           />
+          {formik.errors.email && (
+            <p className={style.inputErrMsg}>{formik.errors.email}</p>
+          )}
         </div>
+
         <div className={style.group}>
           <input
             type="password"
             name="password"
             placeholder="Password"
-            className={style.input}
+            className={`${style.input} ${
+              formik.errors.password ? style.inputErr : ''
+            }`}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             value={formik.values.password}
           />
+          {formik.errors.password && (
+            <p className={style.inputErrMsg}>{formik.errors.password}</p>
+          )}
         </div>
         <div className={style.group}>
           <Button>Sign In</Button>
