c0f7b0a58fb441da19e2523b679e2c2064117911
pocketful <iveta.jac@gmail.com>
2022-07-04 15:53:18 +0300

    submit Login values with formik

--- src/components/Forms/LoginForm.jsx
+++ src/components/Forms/LoginForm.jsx
@@ -1,17 +1,33 @@
+import { useFormik } from 'formik';
 import Button from '../UI/Button/Button';
 import style from './LoginForm.module.css';
 
+const initialValues = {
+  email: '',
+  password: '',
+};
+
 function LoginForm() {
+  const formik = useFormik({
+    initialValues,
+    onSubmit: async (values) => {
+      console.log('submitted values: ', values);
+    },
+  });
+
   return (
     <div className={style.wrapper}>
       <h2>Have an account?</h2>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <div className={style.group}>
           <input
             type="email"
             name="email"
             placeholder="Email"
             className={style.input}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
           />
         </div>
         <div className={style.group}>
@@ -20,6 +36,9 @@ function LoginForm() {
             name="password"
             placeholder="Password"
             className={style.input}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
           />
         </div>
         <div className={style.group}>
