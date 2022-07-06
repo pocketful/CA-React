e5eef2b678dd297f54ad261ac26101e82ce6ca86
pocketful <iveta.jac@gmail.com>
2022-07-06 17:01:02 +0300

    navlinks showing depends on the users login state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,29 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import style from './Header.module.css';
 import Flex from '../UI/Flex/Flex';
 import logo from '../../assets/logo.png';
+import { useAuthCtx } from '../../store/authContext';
 
 function Header() {
+  const { isLoggedIn, logout } = useAuthCtx();
   return (
     <header className={style.header}>
       <Flex>
         <img className={style.img} src={logo} alt="React logo" />
         <nav className={style.nav}>
-          <NavLink to="/login">Login</NavLink>
-          <NavLink to="/register">Register</NavLink>
-          <NavLink exact to="/">Home</NavLink>
-          <NavLink to="/add">Add</NavLink>
-          <NavLink to="/logout">Logout</NavLink>
+          {isLoggedIn && (
+            <>
+              <NavLink exact to="/">Home</NavLink>
+              <NavLink to="/add">Add</NavLink>
+              <Link to="/login" onClick={logout}>Logout</Link>
+            </>
+          )}
+          {!isLoggedIn && (
+            <>
+              <NavLink to="/login">Login</NavLink>
+              <NavLink to="/register">Register</NavLink>
+            </>
+          )}
         </nav>
       </Flex>
     </header>
