e0c2b4f21c5159e285b7d494d4700ebe816a9368
pocketful <iveta.jac@gmail.com>
2022-07-06 16:06:43 +0300

    Register validation with Yup

--- src/components/Forms/RegisterForm.jsx
+++ src/components/Forms/RegisterForm.jsx
@@ -1,4 +1,7 @@
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useState } from 'react';
+import { inputFeedback, inputFeedbackText } from '../../helpers/inputFeedback/inputFeedback';
 import Button from '../UI/Button/Button';
 import style from './Form.module.css';
 
@@ -14,43 +17,59 @@ const initialValues = {
 function RegisterForm() {
   const formik = useFormik({
     initialValues,
+    validationSchema: Yup.object({
+      email: Yup.string().email('invalid email address').required(),
+      password: Yup.string().min(4, 'min 4 characters').max(20).required(),
+      passwordRef: Yup.string()
+        .required('please retype your password')
+        .oneOf([Yup.ref('password'), null], 'passwords must match'),
+    }),
     onSubmit: async (values) => {
       console.log('submitted values: ', values);
     },
   });
+
+  console.log('formik.errors ===', formik.errors);
+
   return (
-    <div className={style.wrapper}>
+    <div>
       <h2>Don't have an account?</h2>
-      <form onSubmit={formik.handleSubmit}>
+      <form onSubmit={formik.handleSubmit} className={style.wrapper}>
         <div className={style.group}>
           <input
             type="email"
             name="email"
             placeholder="Email"
-            className={style.input}
+            className={`${style.input} ${inputFeedback('email', formik)}`}
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.email}
           />
+          {inputFeedbackText('email', formik)}
         </div>
         <div className={style.group}>
           <input
             type="password"
             name="password"
             placeholder="Password"
-            className={style.input}
+            className={`${style.input} ${inputFeedback('password', formik)}`}
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.password}
           />
+          {inputFeedbackText('password', formik)}
         </div>
         <div className={style.group}>
           <input
             type="password"
             name="passwordRef"
             placeholder="Confirm Password"
-            className={style.input}
+            className={`${style.input} ${inputFeedback('passwordRef', formik)}`}
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.passwordRef}
           />
+          {inputFeedbackText('passwordRef', formik)}
         </div>
         <div className={style.group}>
           <Button>Sign Up</Button>
